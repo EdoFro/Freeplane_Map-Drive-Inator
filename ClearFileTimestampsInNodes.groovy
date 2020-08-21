abb924e22ea5cd5f5c0a7864198eef8162d2505d
EdoFro <35700575+EdoFro@users.noreply.github.com>
2020-08-21 10:35:23 -0400

    ClearFileTimeStamp corrected

--- ClearFileTimestampsInNodes.groovy
+++ ClearFileTimestampsInNodes.groovy
@@ -1,22 +1,22 @@
 // @ExecutionModes({ON_SELECTED_NODE="/main_menu/ScriptsEdo/MapDriveInator"})
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+// import java.nio.file.Paths;
+// import java.nio.file.attribute.BasicFileAttributes;
 
 
 def nodo = node
 
 nodo.find{MDI.isLinkToFile(it)}.each{n ->
-    Path                file    =   Paths.get(n.link.file.getAbsolutePath())
-    BasicFileAttributes attr    =   Files.readAttributes(file, BasicFileAttributes.class);
-    n['creationTime'] = (attr.creationTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
-    n['lastAccessTime'] = (attr.lastAccessTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
-    n['lastModifiedTime'] = (attr.lastModifiedTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
-    // n['creationTime'] = null
-    // n['lastAccessTime'] = null
-    // n['lastModifiedTime'] = null
+    // Path                file    =   Paths.get(n.link.file.getAbsolutePath())
+    // BasicFileAttributes attr    =   Files.readAttributes(file, BasicFileAttributes.class);
+    // n['creationTime'] = (attr.creationTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
+    // n['lastAccessTime'] = (attr.lastAccessTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
+    // n['lastModifiedTime'] = (attr.lastModifiedTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
+    n['creationTime'] = null
+    n['lastAccessTime'] = null
+    n['lastModifiedTime'] = null
 }
 
-c.statusInfo =' file timestamps added as attributes '
+c.statusInfo =' timestamps attributes deleted'

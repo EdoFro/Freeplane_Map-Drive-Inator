0b815c2a4abc21b16b444b0e6e1537ea867e2db2
EdoFro <35700575+EdoFro@users.noreply.github.com>
2020-09-25 16:54:00 -0300

    Update GetFileTimestamps.groovy
    look first if file does exist in drive

--- GetFileTimestamps.groovy
+++ GetFileTimestamps.groovy
@@ -8,13 +8,17 @@ import java.nio.file.attribute.BasicFileAttributes;
 def nodo = node
 
 nodo.find{MDI.isLinkToFile(it)}.each{n ->
-    Path                file    =   Paths.get(n.link.file.getAbsolutePath())
-    BasicFileAttributes attr    =   Files.readAttributes(file, BasicFileAttributes.class);
+    if (n.link.file.exists()){
+        Path                file    =   Paths.get(n.link.file.getAbsolutePath())
+        BasicFileAttributes attr    =   Files.readAttributes(file, BasicFileAttributes.class);
 
-    def previousModifiedTime =  n['lastModifiedTime']
-    n['lastModifiedTime'] = (attr.lastModifiedTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
-    n['lastAccessTime'] = (attr.lastAccessTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
-    n['creationTime'] = (attr.creationTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
-    
-    n['modifiedFile'] = n['lastModifiedTime']!=previousModifiedTime?true:null
+        def previousModifiedTime =  n['lastModifiedTime']
+        n['lastModifiedTime'] = (attr.lastModifiedTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
+        n['lastAccessTime'] = (attr.lastAccessTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
+        n['creationTime'] = (attr.creationTime().toInstant().toDate().format('yyyy-MM-dd HH:mm'))
+        
+        n['modifiedFile'] = n['lastModifiedTime']!=previousModifiedTime?true:null
+    } else {
+        MDI.markAsBroken(n,true,false)
+    }
 }

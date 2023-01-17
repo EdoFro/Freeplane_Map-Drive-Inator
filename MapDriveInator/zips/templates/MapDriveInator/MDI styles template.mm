ca2d976f9658768bfd35a0897f83b073fe216555
EdoFro <35700575+EdoFro@users.noreply.github.com>
2023-01-17 18:33:10 -0300

    Create MDI styles template.mm
    Added template

---
+++ MapDriveInator/zips/templates/MapDriveInator/MDI styles template.mm
@@ -0,0 +1,98 @@
+<map version="freeplane 1.11.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node LOCALIZED_TEXT="new_mindmap" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1610381621824" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.008">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" STYLE_REF="file" LAST="false">
+            <script_condition>
+                <script>{node.link?.uri?.scheme == &apos;file&apos;}</script>
+            </script_condition>
+        </conditional_style>
+        <conditional_style ACTIVE="true" STYLE_REF="modifiedFile" LAST="false">
+            <attribute_contains_condition ATTRIBUTE="modifiedFile" VALUE="true"/>
+        </conditional_style>
+    </conditional_styles>
+    <properties show_icon_for_attributes="true" show_notes_in_map="false" BookmarksKeys="{&quot;103&quot;:&quot;ID_1479759080&quot;,&quot;118&quot;:&quot;ID_128687180&quot;,&quot;119&quot;:&quot;ID_1586584839&quot;,&quot;97&quot;:&quot;ID_234882181&quot;}" mapVariable_de_prueba="valor variable 75" show_note_icons="true" fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" ID="ID_1625586047" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_168525555" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" MAX_WIDTH="300 px" MIN_WIDTH="0 cm" COLOR="#000000" BACKGROUND_COLOR="#ffffff" STYLE="as_parent" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" CHILD_NODES_LAYOUT="AUTO" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_168525555" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="Gisha" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/html"/>
+<richcontent CONTENT-TYPE="plain/html" TYPE="DETAILS"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#006666">
+<font NAME="Tahoma" SIZE="7" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" ID="ID_324425992" BACKGROUND_COLOR="#ff9900" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc6600"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode TEXT="baseFolder" ID="ID_463236567" ICON_SIZE="20 pt" BACKGROUND_COLOR="#ffeca9">
+<icon BUILTIN="emoji-1F4BD"/>
+<font BOLD="true"/>
+</stylenode>
+<stylenode TEXT="newFolderImport" ID="ID_1102409980" ICON_SIZE="16 pt" BACKGROUND_COLOR="#ffeca9">
+<icon BUILTIN="emoji-1F4BE"/>
+<font BOLD="true"/>
+</stylenode>
+<stylenode TEXT="freshNew" ID="ID_1031381956" ICON_SIZE="16 pt" BACKGROUND_COLOR="#ace500">
+<icon BUILTIN="emoji-1F195"/>
+</stylenode>
+<stylenode TEXT="movedRenamed" ID="ID_1447774033" ICON_SIZE="16 pt" BACKGROUND_COLOR="#ace500">
+<icon BUILTIN="emoji-1F500"/>
+</stylenode>
+<stylenode TEXT="file" ID="ID_424945004" BORDER_WIDTH="2.5 px" BACKGROUND_COLOR="#bcc6e0" FORMAT="NO_FORMAT">
+<font NAME="Consolas"/>
+</stylenode>
+<stylenode TEXT="file_folder" ID="ID_1323521471" BORDER_WIDTH="3 px">
+<icon BUILTIN="emoji-1F4C1"/>
+</stylenode>
+<stylenode TEXT="missing" ID="ID_395146219" BACKGROUND_COLOR="#f28bb3" BORDER_WIDTH="3 px">
+<icon BUILTIN="broken-line"/>
+</stylenode>
+<stylenode TEXT="modifiedFile" ID="ID_1128490688" BACKGROUND_COLOR="#ffcc00" BORDER_WIDTH="4 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#00659b">
+<icon BUILTIN="emoji-002A-20E3"/>
+<font ITALIC="true"/>
+</stylenode>
+<stylenode TEXT="locked" ID="ID_321324165" COLOR="#e1e1e1" BACKGROUND_COLOR="#6f4e4e" BORDER_WIDTH="2.5 px">
+<icon BUILTIN="emoji-1F512"/>
+</stylenode>
+<stylenode TEXT="file_folder_with_icon" ID="ID_319652685">
+<icon BUILTIN="emoji-1F4CD"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" ID="ID_1463538577" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" ID="ID_473108124" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+</node>
+</map>
